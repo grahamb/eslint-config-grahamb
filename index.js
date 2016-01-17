0dca05a2673913671750c568a2f19f788978df67
Graham Ballantyne <hello@grahamballantyne.com>
2016-01-16 20:45:12 -0800

    reduce no-usused-vars to warning

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ module.exports = {
     "no-self-compare": 0,       // disallow comparisons where both sides are exactly the same (off by default)
     "no-sequences": 2,          // disallow use of comma operator
     "no-throw-literal": 0,      // restrict what can be thrown as an exception (off by default)
-    "no-unused-expressions": 2, // disallow usage of expressions in statement position
+    "no-unused-expressions": 1, // disallow usage of expressions in statement position
     "no-void": 0,               // disallow use of void operator (off by default)
     "no-warning-comments": 0,   // disallow usage of configurable warning terms in comments, e.g. TODO or FIXME (off by default)
     "no-with": 2,               // disallow use of the with statement
